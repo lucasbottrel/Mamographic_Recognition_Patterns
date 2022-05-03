427597bdd0a10537fb9ea12cfac4159f4ff17d60
Lucas Bottrel <lucas.bottrel@levty.com>
2022-05-02 23:12:15 -0300

    Added frames and file explorer label (incompleted)
    Co-authored-by: ViniciusMendes2001 <ViniciusMendes2001@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
+from tkinter import filedialog
+import funcoesMenu as menu
 
+# MAIN FRAME
 root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.wm_title("Mamographic Recognition Patterns")
+root.geometry('1280x720')
+
+# TOP BAR
+menu.menuFunc(root)
+
+# ÁREA DA IMAGEM
+topFrm = LabelFrame(root, text="imagem")
+topFrm.place(relwidth=1, relheight=0.75)
+topFrm.configure(background='white')
+
+# ÁREA DE LOG
+bottomFrm = LabelFrame(root, text="terminal", fg='#fff')
+bottomFrm.place(relwidth=1, relheight=0.25, rely=0.75)
+bottomFrm.configure(background='black')
+
 root.mainloop()

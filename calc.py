13c3e01e5e555d811ac1314483c59c7ac3e9c1e4
Lucas Bottrel <lucas.bottrel@levty.com>
2022-05-06 11:49:08 -0300

    update haralick screen

--- calc.py
+++ calc.py
@@ -6,4 +6,17 @@ print(skimage.__version__)
 
 def openHaralick():
     haralickScreen = Tk()
+    haralickScreen.wm_title("Haralick Calcs")
+    haralickScreen.geometry('500x400')
+    
+    haralickScreen.config(background="white");
+    
+    homog = Label(haralickScreen, text="Descritores de Haralick", font=('',24))
+    homog.config(background="white")
+    homog.place(relx=0,y=20)
+    
+    homog = Label(haralickScreen, text="Homogeneidade")
+    homog.config(background="white")
+    homog.place(x=40,y=70)
+    
     haralickScreen.mainloop()

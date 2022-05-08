db4804d03ede5d384b63d6c22e80f74dd939da0c
Lucas Bottrel Lopes de Moura <lucas.bottrel@levty.com>
2022-05-08 20:14:10 -0300

    changes on haralick calcs
    Co-authored-by: ViniciusMendes2001 <ViniciusMendes2001@users.noreply.github.com>

--- haralick.py
+++ haralick.py
@@ -1,12 +1,51 @@
+from skimage import data
+from skimage.feature import graycomatrix, graycoprops
+from skimage.measure import shannon_entropy
+from skimage.morphology import disk, ball
+from skimage import *
 import numpy as np
 
-#function to calculate the entropy of the image
-def entropy(im):
-    # Compute normalized histogram 
-    p = np.array([(im==v).sum() for v in range(256)])
-    p = p/p.sum()
-    # Compute e = -sum(p(g)*log2(p(g)))
-    e = -(p[p>0]*np.log2(p[p>0])).sum()
+def haralick_calcs(img_calc):
     
-    return e
-
+    gcm_1 = graycomatrix(img_calc, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4])
+    gcm_2 = graycomatrix(img_calc, [1, 2], [0, np.pi/4, np.pi/2, 3*np.pi/4])
+    gcm_4 = graycomatrix(img_calc, [1, 4], [0, np.pi/4, np.pi/2, 3*np.pi/4])
+    gcm_8 = graycomatrix(img_calc, [1, 8], [0, np.pi/4, np.pi/2, 3*np.pi/4])
+    gcm_16 = graycomatrix(img_calc, [1, 16], [0, np.pi/4, np.pi/2, 3*np.pi/4])
+    
+    haralick_1 = {
+        'matriz_cooc': gcm_1,
+        'entropia': shannon_entropy(gcm_1, base=2),
+        'energy': graycoprops(gcm_1, 'energy')[0,0],
+        'homogeneity': graycoprops(gcm_1, 'homogeneity')[0,0]
+    }
+    
+    haralick_2 = {
+        'matriz_cooc': gcm_2,
+        'entropia': shannon_entropy(gcm_2, base=2),
+        'energy': graycoprops(gcm_2, 'energy')[0,0],
+        'homogeneity': graycoprops(gcm_2, 'homogeneity')[0,0]
+    }
+    
+    haralick_4 = {
+        'matriz_cooc': gcm_4,
+        'entropia': shannon_entropy(gcm_4, base=2),
+        'energy': graycoprops(gcm_4, 'energy')[0,0],
+        'homogeneity': graycoprops(gcm_1, 'homogeneity')[0,0]
+    }
+    
+    haralick_8 = {
+        'matriz_cooc': gcm_8,
+        'entropia': shannon_entropy(gcm_8, base=2),
+        'energy': graycoprops(gcm_8, 'energy')[0,0],
+        'homogeneity': graycoprops(gcm_8, 'homogeneity')[0,0]
+    }
+    
+    haralick_16 = {
+        'matriz_cooc': gcm_16,
+        'entropia': shannon_entropy(gcm_16, base=2),
+        'energy': graycoprops(gcm_16, 'energy')[0,0],
+        'homogeneity': graycoprops(gcm_16, 'homogeneity')[0,0]
+    }
+    
+    return [haralick_1, haralick_2, haralick_4, haralick_8, haralick_16]
--- main.py
+++ main.py
@@ -1,30 +1,66 @@
-from fileinput import filename
 from tkinter import *
-from tkinter import ttk
-from PIL import Image, ImageTk
 from tkinter import filedialog
-from skimage.io import imread
-from calc import openHaralick
-from haralick import entropy
+from imageio import imread
+from skimage import data
+from skimage.feature import graycomatrix, graycoprops
+from skimage.measure import shannon_entropy
+from skimage.morphology import disk, ball
+from skimage import *
+import numpy as np
+from PIL import Image, ImageTk
+
+img_calc = None
+
+def clear_label_image():
+    imgFrm.config(image = '')
 
 def upload_file():
     clear_label_image()
+    
     global img
-    f_types = (
-        ("Image files", "*.png *.jpeg"), ("All files", "*.*")
-    )
+    global img_calc
+    
+    f_types = (("Image files", "*.png *.jpeg"), ("All files", "*.*"))
     filename = filedialog.askopenfilename(filetypes=f_types)
-    im = imread(filename)
-    print(entropy(im))
+    
+    img_calc = imread(filename)
+    
     img = (Image.open(filename))
-    resized_image= img.resize((400,400), Image.ANTIALIAS)
-    img = ImageTk.PhotoImage(resized_image) 
+    img= ImageTk.PhotoImage(img.resize((400,400), Image.ANTIALIAS))
     imgFrm.config(image=img) 
     imgFrm.place(relx=0.5, rely=0.5)
+    
     imgFrm.pack()
 
-def clear_label_image():
-    imgFrm.config(image = '');
+def openHaralick():
+    haralickScreen = Tk()
+    haralickScreen.wm_title("Haralick Calcs")
+    haralickScreen.geometry('500x400')
+    
+    haralickScreen.config(background="white");
+    
+    title = Label(haralickScreen, text="Descritores de Haralick", font=('',24), anchor=CENTER)
+    title.config(background="white")
+    title.place(x=30,y=20)
+    
+    haralick_calcs = haralick_calcs(img_calc)
+    
+    c1 = Label(haralickScreen, text="C1 ", font=('',14))
+    c1.config(background="white")
+    c1.place(x=30,y=80)
+    
+    homog = Label(haralickScreen, text="Homogeneidade: " + str(round(homog_1,2)), font=('',12))
+    homog.config(background="white")
+    homog.place(x=30,y=120)
+    
+    energ = Label(haralickScreen, text="Energia: " + str(round(energ_1,2)), font=('',12))
+    energ.config(background="white")
+    energ.place(x=30,y=160)
+    
+    entrop = Label(haralickScreen, text="Entropia: " + str(round(entrop_1,2)), font=('',12))
+    entrop.config(background="white")
+    entrop.place(x=30,y=200)
+
 
 # MAIN FRAME
 root = Tk()
@@ -54,7 +90,7 @@ topSpace.place(relwidth=1, relheight=0.15)
 topSpace.configure(background='white')
 
 topFrm = Frame(root)
-imgFrm =Label(topFrm,image='')
+imgFrm = Label(topFrm,image='')
 topFrm.place(relwidth=1, relheight=0.75, rely=0.15)
 topFrm.configure(background='white')
 

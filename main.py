c11dea32f5a073ed600edaf4415039d2c094c7b9
Lucas Bottrel <lucas.bottrel@levty.com>
2022-05-10 11:01:13 -0300

    Completed haralick descriptors

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from ctypes.wintypes import RGB
 import time
 from tkinter import *
 from tkinter import filedialog
+from turtle import left
 from imageio import imread
 from skimage import data
 from skimage.feature import graycomatrix, graycoprops
@@ -37,45 +39,92 @@ def upload_file():
     imgFrm.pack()
 
 def openHaralick():
-    
     haralickScreen = Tk()
     haralickScreen.wm_title("Haralick Calcs")
-    haralickScreen.geometry('500x400')
+    haralickScreen.geometry('1120x400')
     
     haralickScreen.config(background="white");
     
-    title = Label(haralickScreen, text="Descritores de Haralick", font=('',24), anchor=CENTER)
-    title.config(background="white")
+    title = Label(haralickScreen, text="Descritores de Haralick", font=('',24), anchor=CENTER, background='#5559fd' ,fg='white')
     title.place(x=30,y=20)
     
     start = time.time()
     
     haralick_results = haralick_calcs(img_calc)
     
-    global execution_time
-    execution_time = time.time() - start
-    
-    c1 = Label(haralickScreen, text="C1 ", font=('',14))
-    c1.config(background="white")
+    c1 = Label(haralickScreen, text="C1 ", font=('',14), background="white", fg='#5559fd')
     c1.place(x=30,y=80)
     
-    homog = Label(haralickScreen, text="Homogeneidade: " + str(round(haralick_results[0].homogeneity,2)), font=('',12))
-    homog.config(background="white")
-    homog.place(x=30,y=120)
+    homog_c1 = Label(haralickScreen, text="Homogeneidade: " + str(round(haralick_results[0].homogeneity,3)), font=('',12), background="white")
+    homog_c1.place(x=30,y=120)
     
-    energ = Label(haralickScreen, text="Energia: " + str(round(haralick_results[0].energy,2)), font=('',12))
-    energ.config(background="white")
-    energ.place(x=30,y=160)
+    energ_c1 = Label(haralickScreen, text="Energia: " + str(round(haralick_results[0].energy,3)), font=('',12), background="white")
+    energ_c1.place(x=30,y=160)
     
-    entrop = Label(haralickScreen, text="Entropia: " + str(round(haralick_results[0].entropy,2)), font=('',12))
-    entrop.config(background="white")
-    entrop.place(x=30,y=200)
+    entrop_c1 = Label(haralickScreen, text="Entropia: " + str(round(haralick_results[0].entropy,3)), font=('',12), background="white")
+    entrop_c1.place(x=30,y=200)
+    
+    c2 = Label(haralickScreen, text="C2 ", font=('',14), background="white", fg='#5559fd')
+    c2.place(x=250,y=80)
+    
+    homog_c2 = Label(haralickScreen, text="Homogeneidade: " + str(round(haralick_results[1].homogeneity,3)), font=('',12), background='white')
+    homog_c2.place(x=250,y=120)
+    
+    energ_c2 = Label(haralickScreen, text="Energia: " + str(round(haralick_results[1].energy,3)), font=('',12), background="white")
+    energ_c2.place(x=250,y=160)
+    
+    entrop_c2 = Label(haralickScreen, text="Entropia: " + str(round(haralick_results[1].entropy,3)), font=('',12), background="white")
+    entrop_c2.place(x=250,y=200)
+    
+    c4 = Label(haralickScreen, text="C4 ", font=('',14), background="white", fg='#5559fd')
+    c4.place(x=470,y=80)
+    
+    homog_c4 = Label(haralickScreen, text="Homogeneidade: " + str(round(haralick_results[2].homogeneity,3)), font=('',12), background='white')
+    homog_c4.place(x=470,y=120)
+    
+    energ_c4 = Label(haralickScreen, text="Energia: " + str(round(haralick_results[2].energy,3)), font=('',12), background="white")
+    energ_c4.place(x=470,y=160)
+    
+    entrop_c4 = Label(haralickScreen, text="Entropia: " + str(round(haralick_results[2].entropy,3)), font=('',12), background="white")
+    entrop_c4.place(x=470,y=200)
+    
+    c8 = Label(haralickScreen, text="C8 ", font=('',14), background="white", fg='#5559fd')
+    c8.place(x=690,y=80)
+    
+    homog_c8 = Label(haralickScreen, text="Homogeneidade: " + str(round(haralick_results[3].homogeneity,3)), font=('',12), background='white')
+    homog_c8.place(x=690,y=120)
+    
+    energ_c8 = Label(haralickScreen, text="Energia: " + str(round(haralick_results[3].energy,3)), font=('',12), background="white")
+    energ_c8.place(x=690,y=160)
+    
+    entrop_c8 = Label(haralickScreen, text="Entropia: " + str(round(haralick_results[3].entropy,3)), font=('',12), background="white")
+    entrop_c8.place(x=690,y=200)
+    
+    c16 = Label(haralickScreen, text="C16 ", font=('',14), background="white", fg='#5559fd')
+    c16.place(x=910,y=80)
+    
+    homog_c16 = Label(haralickScreen, text="Homogeneidade: " + str(round(haralick_results[4].homogeneity,3)), font=('',12), background='white')
+    homog_c16.place(x=910,y=120)
+    
+    energ_c16 = Label(haralickScreen, text="Energia: " + str(round(haralick_results[4].energy,3)), font=('',12), background="white")
+    energ_c16.place(x=910,y=160)
+    
+    entrop_c16 = Label(haralickScreen, text="Entropia: " + str(round(haralick_results[4].entropy,3)), font=('',12), background="white")
+    entrop_c16.place(x=910,y=200)
+    
+    execution_time = time.time() - start
+    
+    # ÁREA DE LOG
+    bottomFrm = LabelFrame(haralickScreen, fg='#fff', text='Tempo de Execução', background='black')
+    execTimeFrm = Label(bottomFrm, text=str(round(execution_time,4)) + ' ms', fg='white', background='black', font=('',11))
+    execTimeFrm.place(x=10, y=5)
+    bottomFrm.place(relwidth=1, relheight=0.2, rely=0.8)
 
 
 # MAIN FRAME
 root = Tk()
 root.wm_title("Mamographic Recognition Patterns")
-root.geometry('1280x720')
+root.geometry('1280x600')
 
 # TOP BAR
 menubar = Menu(root)
@@ -105,10 +154,7 @@ topFrm.place(relwidth=1, relheight=0.75, rely=0.15)
 topFrm.configure(background='white')
 
 # ÁREA DE LOG
-bottomFrm = LabelFrame(root, fg='#fff', text='Tempo de Execução')
-execTimeFrm = Label(bottomFrm, text=execution_time, fg='white', background='black', font=('',11))
-execTimeFrm.place(x=10, y=10)
+bottomFrm = LabelFrame(root, fg='#fff', text='Classificação da Imagem', background='black')
 bottomFrm.place(relwidth=1, relheight=0.1, rely=0.9)
-bottomFrm.configure(background='black')
 
 root.mainloop()
